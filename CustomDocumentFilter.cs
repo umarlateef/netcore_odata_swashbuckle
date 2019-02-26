a29c0e98529711ddeedb987bbad076836d96b745
umarlateef <umar.sd@hotmail.com>
2019-02-26 10:38:54 -0600

    Update CustomDocumentFilter.cs

--- CustomDocumentFilter.cs
+++ CustomDocumentFilter.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace SPrime.Services.App.Filters
+namespace Services.App.Filters
 {
     public class CustomSwaggerDocumentFilter : IDocumentFilter
     {
